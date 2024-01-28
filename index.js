0a35dba94d4ac5c3befc9cb6c4293d28923ea24c
Naveen A <121672275+naveen20022002@users.noreply.github.com>
2024-01-28 15:53:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'https://hotelbooking2.onrender.com',
+    origin: 'https://localhost:4000',
 }));
 // app.use(cors());
 
