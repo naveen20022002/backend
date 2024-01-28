2947598fc4c26ad12f6ef3d94e2022db1a806b03
Naveen A <121672275+naveen20022002@users.noreply.github.com>
2024-01-28 16:21:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'https:/localhost:5173',
+    origin: 'https://localhost:5173',
 }));
 // app.use(cors());
 
