e10a67c094cdaae3c724828d0ff678234d51725a
Naveen A <121672275+naveen20022002@users.noreply.github.com>
2024-01-28 17:06:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ const jwtSecret = 'kljdklajsdkfkaf';
 app.use('/uploads', express.static(__dirname +'/uploads'));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // app.use(cors({
 //     credentials: true,
 //     origin: 'http://localhost:5173',
