40e0250624d9deb1bb89e48805b4442a87d820bb
Naveen A <121672275+naveen20022002@users.noreply.github.com>
2024-01-28 17:15:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ app.use('/uploads', express.static(__dirname +'/uploads'));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
-// app.use(cors({
-//     credentials: true,
-//     origin: 'http://localhost:5173',
-// }));
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+}));
 // app.use(cors());
 
 function getUserDataFromReq(req){
